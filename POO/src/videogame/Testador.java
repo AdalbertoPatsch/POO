59b3dd5e06e3df4445e3ac2ba2025afb73a100ad
crfranco <crfranco@crfranco.laptop>
2015-04-17 19:40:25 -0300

    polimorfismo

--- POO/src/videogame/Testador.java
+++ POO/src/videogame/Testador.java
@@ -3,18 +3,19 @@ package videogame;
 public class Testador {
 
 	public static void main(String[] args) {
-		
-		Wii wii = new Wii("Core 2 duo",20,Midia.DVD);
-		
+
+		Wii wii = new Wii("Core 2 duo", 20, Midia.DVD);
+
+		VideoGame vg = new
+
 		System.out.println(wii);
-		
-		
+
 		Xbox xbox = new Xbox("celeron", 120, Midia.DVD, "Xbox 360");
-		
+
 		Kinect ximbinha = new Kinect("Kinect");
-		
+
 		xbox.setKinect(ximbinha);
-		
+
 		System.out.println(xbox);
 
 	}
