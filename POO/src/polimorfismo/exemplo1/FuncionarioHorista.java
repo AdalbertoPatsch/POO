3b9f2b2f3037ab614784304455bce9ea1b4daf4e
crfranco <crfranco@crfranco.laptop>
2015-04-18 09:34:47 -0300

    Exemplo polimorfismo

--- POO/src/polimorfismo/exemplo1/FuncionarioHorista.java
+++ POO/src/polimorfismo/exemplo1/FuncionarioHorista.java
@@ -1,44 +1,17 @@
 package polimorfismo.exemplo1;
 
-
-
-public class FuncionarioHorista extends Funcionario{
+public class FuncionarioHorista extends Funcionario {
 
 	private double nroHoras;
 	private double valorHora;
-	
-	
+
 	public FuncionarioHorista(int matricula, String nome, double nroHoras,
 			double valorHora) {
 		super(matricula, nome);
 		this.nroHoras = nroHoras;
 		this.valorHora = valorHora;
-		
-		
+
 	}
-
-	
-
-	public double getNroHoras() {
-		return nroHoras;
-	}
-
-
-	public void setNroHoras(double nroHoras) {
-		this.nroHoras = nroHoras;
-	}
-
-
-	public double getValorHora() {
-		return valorHora;
-	}
-
-
-	public void setValorHora(double valorHora) {
-		this.valorHora = valorHora;
-	}
-
-
 
 	@Override
 	public double calcularSalario() {
@@ -46,5 +19,20 @@ public class FuncionarioHorista extends Funcionario{
 		return salario;
 	}
 
-	
+	public double getNroHoras() {
+		return nroHoras;
+	}
+
+	public void setNroHoras(double nroHoras) {
+		this.nroHoras = nroHoras;
+	}
+
+	public double getValorHora() {
+		return valorHora;
+	}
+
+	public void setValorHora(double valorHora) {
+		this.valorHora = valorHora;
+	}
+
 }
--- POO/src/polimorfismo/exemplo1/Testador.java
+++ POO/src/polimorfismo/exemplo1/Testador.java
@@ -3,21 +3,22 @@ package polimorfismo.exemplo1;
 public class Testador {
 
 	public static void main(String[] args) {
-		
-		FuncionarioHorista fh = new FuncionarioHorista(1,"Juquinha",160,20);
-		fh.calcularSalario();
+
+		CalculadoraDeSalario cds = new CalculadoraDeSalario();
+
+		FuncionarioHorista fh = new FuncionarioHorista(1, "Juquinha", 160, 20);
+		cds.calcularSalario(fh);
 		System.out.println(fh.toString());
-		
-		
-		FuncionarioComissionado fc = new FuncionarioComissionado(2,"Anne", 2000,145);
-		fc.calcularSalario();
+
+		FuncionarioComissionado fc = new FuncionarioComissionado(2, "Anne",
+				2000, 145);
+		cds.calcularSalario(fc);
 		System.out.println(fc);
-		
-		
-		FuncionarioNormal fn = new FuncionarioNormal(3,"Juliana",4500);
-		fn.calcularSalario();
+
+		FuncionarioNormal fn = new FuncionarioNormal(3, "Juliana", 4500);
+		cds.calcularSalario(fn);
 		System.out.println(fn);
-		
+
 	}
 
 }
